f9002a824427914b81001ae100a065dc446a902d
mashagabri <mashagabri@outlook.com>
2025-04-07 13:25:54 +0300

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <>
+      <div class="page">
+        <div class="page__content">
+          <Header />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,4 @@
+function Footer() {
+  return <footer>FOOTER</footer>;
+}
+export default Footer;

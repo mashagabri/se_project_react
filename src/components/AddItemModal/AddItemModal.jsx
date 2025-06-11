9c621b12c9e4accf15a13791a483a57452b08506
mashagabri <mashagabri@outlook.com>
2025-06-11 17:23:15 +0300

    done

--- src/components/AddItemModal/AddItemModal.jsx
+++ src/components/AddItemModal/AddItemModal.jsx
@@ -1,10 +1,3 @@
-// import "./AddItemModal.css";
-
-// function AddItemModal({ modalWithForm }) {
-//   return modalWithForm;
-// }
-// export default AddItemModal;
-
 import "./AddItemModal.css";
 
 function AddItemModal({ modalWithForm }) {
